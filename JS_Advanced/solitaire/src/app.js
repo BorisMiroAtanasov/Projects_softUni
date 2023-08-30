3a1850c4d6841571b6b1ec377fe59169584bb2bb
BorisMiroAtanasov <borisatanasov84@abv.bg>
2023-08-30 23:34:23 +0300

    update

--- JS_Advanced/solitaire/src/app.js
+++ JS_Advanced/solitaire/src/app.js
@@ -1,12 +1,31 @@
 import { Deck, Foundation, Pile, Stock, Waste, suits } from "./card.js";
-import { createDeck, dealDeck, shufelDdeck } from "./util.js";
+import { createDeck, shufelDdeck } from "./util.js";
 
 start();
 
 
 function start(){
+    const state = {
+        stock: new Stock(),
+        waste: new Waste(),
+        foundation: {
+            [suits.Clubs] : new Foundation([],suits.Clubs),
+            [suits.Diamonds] : new Foundation([],suits.Diamonds),
+            [suits.Heards] : new Foundation([],suits.Heards),
+            [suits.Spades] : new Foundation([],suits.Spades),
+        },
+        piles: [
+            new Pile(),
+            new Pile(),
+            new Pile(),
+            new Pile(),
+            new Pile(),
+            new Pile(),
+            new Pile(),
+        ]
+    };
     
-    
+    console.log(state);
     const deck = createDeck();
     shufelDdeck(deck);
     shufelDdeck(deck);
@@ -15,10 +34,12 @@ function start(){
     shufelDdeck(deck);
     shufelDdeck(deck);
     shufelDdeck(deck);
+
     console.log(deck);
 
-    const state = dealDeck(deck);
+    // const state = dealDeck(deck);
     
-    console.log(state);
+   
 }
+
 
--- JS_Advanced/solitaire/src/util.js
+++ JS_Advanced/solitaire/src/util.js
@@ -37,46 +37,36 @@ export function createDeck(){
  * @param {Deck} deck 
  * @returns {GameState}
  */
+
 export function dealDeck(deck){
-    const state = {
-        stock: new Stock(),
-        waste: new Waste(),
-        foundation: {
-            [suits.Clubs] : new Foundation([],suits.Clubs),
-            [suits.Diamonds] : new Foundation([],suits.Diamonds),
-            [suits.Heards] : new Foundation([],suits.Heards),
-            [suits.Spades] : new Foundation([],suits.Spades),
-        },
-        piles: [
-            new Pile(),
-            new Pile(),
-            new Pile(),
-            new Pile(),
-            new Pile(),
-            new Pile(),
-            new Pile(),
-        ]
-        
-    }
-    for (let i= 0 ; i < 7; i++){
-        const pile = state.piles[i];
-        for (let j = 0 ; j <= i ; j++){
-            pile.cards.push(deck.cards.pop())
-        }
-
-        pile.top.faceUp = true
-    }
-
+    
 }
+// export function dealDeck(deck){
+    
+        
+    // }
+    // for (let i= 0 ; i < 7; i++){
+    //     const pile = state.piles[i];
+    //     for (let j = 0 ; j <= i ; j++){
+    //        // pile.cards.push(deck.cards.pop())
+    //        pile.cards.push(deck.cards.pop())
+    //     }
 
-/**
- * @typedef {Object} GameState
- * @property {Stock} stock
- * @property {Waste} waste
- * @property {Object} foundation
- * @property {Foundation} foundation.clubs
- * @property {Foundation} foundation.diamonds
- * @property {Foundation} foundation.hearts
- * @property {Foundation} foundation.spades
- * @property {[Pile,Pile,Pile,Pile,Pile,Pile,Pile]} piles
- */
+    //     pile.top.faceUp = true
+    // }
+    // state.stock.cards.push(...deck.cards);
+    // return state;
+
+// }
+
+// /**
+//  * @typedef {Object} GameState
+//  * @property {Stock} stock
+//  * @property {Waste} waste
+//  * @property {Object} foundation
+//  * @property {Foundation} foundation.clubs
+//  * @property {Foundation} foundation.diamonds
+//  * @property {Foundation} foundation.hearts
+//  * @property {Foundation} foundation.spades
+//  * @property {[Pile,Pile,Pile,Pile,Pile,Pile,Pile]} piles
+//  */
